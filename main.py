c60143ad343e5181537a6fd7d62804fdb67c0692
KrivosheyYuriy <lonespy@yandex.ru>
2023-02-09 15:00:07 +0300

    det_image function added

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import sys
-
 from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication
+import requests
+from PIL import Image
+from io import BytesIO
+
+
+def get_image(toponym_longitude, toponym_lattitude, spn, l):
+    map_params = {
+        "ll": ','.join([toponym_longitude, toponym_lattitude]),
+        "spn": ",".join(spn),
+        "l": l, 'pt': ",".join([toponym_longitude, toponym_lattitude])
+    }
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    response = requests.get(map_api_server, params=map_params)
+    img = Image.open(BytesIO(
+        response.content))
+    img.show()
+    img.save('1.png')
 
 
 class Maps(QMainWindow):
