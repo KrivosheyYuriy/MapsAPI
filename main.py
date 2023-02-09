80b13959257c3094cd044a0df999d011853ff986
KrivosheyYuriy <lonespy@yandex.ru>
2023-02-09 15:28:48 +0300

    saved data

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import random
 import sys
-from io import BytesIO
-# Этот класс поможет нам сделать картинку из потока байт
-from PyQt5.QtCore import Qt
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication
 import requests
 from PIL import Image
+from io import BytesIO
+from PyQt5.QtCore import Qt
 
 
-def get_image(toponym_longitude, toponym_lattitude, spn):
+def get_image(toponym_lattitude, toponym_longitude, spn, l):
     map_params = {
-        "ll": ','.join([toponym_longitude, toponym_lattitude]),
-        "spn": ",".join(spn),
-        "l": random.choice(["map", "sat"]), 'pt': ",".join([toponym_longitude, toponym_lattitude])
+        "ll": ','.join([toponym_lattitude, toponym_longitude]),
+        "spn": ",".join([spn, spn]),
+        "l": l, 'pt': ",".join([toponym_longitude, toponym_lattitude])
     }
     map_api_server = "http://static-maps.yandex.ru/1.x/"
     response = requests.get(map_api_server, params=map_params)
@@ -22,13 +22,32 @@ def get_image(toponym_longitude, toponym_lattitude, spn):
 
 
 def key_press_event(event):
-    global spn
+    global spn, lantitude, longitude
     if event.key() == Qt.Key_PageUp:
         spn += 0.0005
     elif event.key() == Qt.Key_PageDown:
         if spn > 0.0005:
             spn -= 0.0005
+    elif event.key() == Qt.Key_Right:
+        longitude += spn
+    elif event.key() == Qt.Key_Left:
+        longitude += spn
+    elif event.key() == Qt.Key_Down:
+        lantitude -= spn
+    elif event.key() == Qt.Key_Up:
+        lantitude += spn
 
 
-get_image('38.910410', '45.036155', ['0.005', '0.005'])
-spn = 0
+class Maps(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Maps.ui', self)
+
+
+if __name__ == '__main__':
+    lantitude, longitude = 45.036114, 38.910410
+    spn = 0.0005
+    application = QApplication(sys.argv)
+    window = Maps()
+    window.show()
+    sys.exit(application.exec())
