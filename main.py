f2440dea96ba7d75599377b812a3a01adc1727b3
DmitryO325 <oberemokdima05@gmail.com>
2023-02-09 15:00:29 +0300

    Task 2

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ import sys
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtCore import Qt
 
 
 class Maps(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('Maps.ui', self)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            pass  # увеличить масштаб на какое-то значение
+
+        if event.key() == Qt.Key_PageDown:
+            pass  # уменьшить масштаб на какое-то значение
 
 
 if __name__ == '__main__':
