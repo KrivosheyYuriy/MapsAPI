404f566877b96e4114401b7dbc19489939fb87a1
DmitryO325 <oberemokdima05@gmail.com>
2023-02-09 15:28:22 +0300

    Task 1 is ready!

--- Task 1/main.py
+++ Task 1/main.py
@@ -1,10 +1,10 @@
 import sys
 from PyQt5 import uic
+from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QMainWindow, QApplication
 import requests
 from PIL import Image
 from io import BytesIO
-from PyQt5.QtCore import Qt
 
 
 def get_image(toponym_lattitude, toponym_longitude, spn, l):
@@ -17,25 +17,20 @@ def get_image(toponym_lattitude, toponym_longitude, spn, l):
     response = requests.get(map_api_server, params=map_params)
     img = Image.open(BytesIO(
         response.content))
-    img.show()
     img.save('1.png')
-
-
-def key_press_event(event):
-    global spn, lantitude, longitude
-    if event.key() == Qt.Key_PageUp:
-        spn += 0.0005
-    elif event.key() == Qt.Key_PageDown:
-        if spn > 0.0005:
-            spn -= 0.0005
-    elif event.key() == Qt.Key_Right:
-        pass
 
 
 class Maps(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('Maps.ui', self)
+
+        self.add_map()
+
+    def add_map(self):
+        get_image('38.910410', '45.036114', '0.0005', 'map')
+        pixmap = QPixmap('1.png')
+        self.label.setPixmap(pixmap)
 
 
 if __name__ == '__main__':
