8629517d86c49cf373e540ac33ca5d65d093f39c
DmitryO325 <oberemokdima05@gmail.com>
2023-02-09 15:58:07 +0300

    Task 2 is ready!

--- Task 2/main.py
+++ Task 2/main.py
@@ -12,8 +12,7 @@ def get_image(toponym_lattitude, toponym_longitude, z, l):
     map_params = {
         "ll": ','.join([toponym_lattitude, toponym_longitude]),
         "z": z,
-        "l": l,
-        'pt': ",".join([toponym_lattitude, toponym_longitude])
+        "l": l
     }
     map_api_server = "http://static-maps.yandex.ru/1.x/"
     response = requests.get(map_api_server, params=map_params)
@@ -41,7 +40,6 @@ class Maps(QMainWindow):
                 z -= 1
 
         self.add_map()
-        print(z)
 
     def add_map(self):
         get_image(longitude, lantitude, z, 'map')
