359a67dcd18977712dea3a1c009cc2d937efd0a1
DmitryO325 <oberemokdima05@gmail.com>
2023-02-09 14:49:39 +0300

    FrontEnd

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-pass
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+
+class Maps(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Maps.ui', self)
+
+
+if __name__ == '__main__':
+    application = QApplication(sys.argv)
+    window = Maps()
+    window.show()
+    sys.exit(application.exec())
